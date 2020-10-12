2e8e0ce8b80cedbb4db53eda2f53827b81f91bdb
Grant Surlyn <grant@sunflowerseastar.com>
2020-10-12 13:19:38 -0700

    wire in apl placePiece helper, add partial "lift" and place logic

--- src/index.js
+++ src/index.js
@@ -8,8 +8,11 @@ import {
   getNumberOfMills,
   getNumberOfPieces,
   openPointsAdjacentToPiece,
+  atest
 } from './apl';
-import { boardDbAtPhase2, gridIndexToAplIndex } from './utility';
+import { boardDbAtPhase2, connectedPointsGraph, gridIndexToAplIndex } from './utility';
+
+// atest([1,5])
 
 const initialDb = {
   phase: 1,
@@ -60,7 +63,6 @@ const boardsView = defView(db, ['boards'], (boards) => [
           'span.point',
           {
             onclick: () => {
-              console.log('db.deref()', db.deref());
               const currentAction = action.deref();
               const currentOpponent = opponent.deref();
               const currentPhase = phase.deref();
@@ -70,7 +72,7 @@ const boardsView = defView(db, ['boards'], (boards) => [
               const clickedOnOpponent = pieceAtPoint === currentOpponent;
               const clickedOnOwnPiece = pieceAtPoint === currentTurn;
 
-              if (currentAction === 'place' && !pieceAtPoint) {
+              if (currentPhase === 1 && currentAction === 'place' && !pieceAtPoint) {
                 const newBoard = aplPlacePiece(boards[currentTurn], aplIndex);
                 boardsCursor.resetIn(currentTurn, newBoard);
 
@@ -87,6 +89,9 @@ const boardsView = defView(db, ['boards'], (boards) => [
                 } else {
                   changeTurn();
                 }
+              } else if (currentPhase === 2 && currentAction === 'place' && !pieceAtPoint) {
+                console.log('place piece?')
+                // TODO see if this piece is in "possiblePlaces"
               } else if (currentAction === 'remove' && clickedOnOpponent) {
                 const newBoard = aplRemovePiece(
                   boards[currentOpponent],
@@ -97,28 +102,23 @@ const boardsView = defView(db, ['boards'], (boards) => [
                 action.reset('place');
                 changeTurn();
               } else if (currentAction === 'lift' && clickedOnOwnPiece) {
-                console.log('yea lift', aplIndex);
                 const openPoints = openPointsAdjacentToPiece(
                   boards[currentTurn],
                   boards[currentOpponent],
-                  aplIndex
+                  connectedPointsGraph[aplIndex]
                 );
+
                 console.log('openPoints', openPoints);
+
+                if (!!openPoints.length) {
+                  console.log('yes open points - make these possible places')
+                  action.reset('place');
+                  // create db of "possiblePlaces" or similar, and update that here
+                } else {
+                  console.log('no open moves')
+                  // do nothing, or maybe provide feedback to the user
+                }
               }
-
-              // TODO check number of pieces for each player
-              const boardAfterMove = boardsCursor.deref();
-              const numberOfPiecesCurrent = getNumberOfPieces(
-                boardAfterMove[currentTurn]
-              );
-              const numberOfPiecesOpponent = getNumberOfPieces(
-                boardAfterMove[currentOpponent]
-              );
-
-              numPiecesOnBoardCursor.reset({
-                [currentTurn]: numberOfPiecesCurrent,
-                [currentOpponent]: numberOfPiecesOpponent,
-              });
 
               const currentNumPlaced = numPiecesPlacedCursor.deref();
 
@@ -133,7 +133,22 @@ const boardsView = defView(db, ['boards'], (boards) => [
               }
 
               if (currentPhase === 2) {
-                console.log('yep in phase 2');
+                // console.log('yep in phase 2');
+
+
+                const boardAfterMove = boardsCursor.deref();
+                const numberOfPiecesCurrent = getNumberOfPieces(
+                  boardAfterMove[currentTurn]
+                );
+                const numberOfPiecesOpponent = getNumberOfPieces(
+                  boardAfterMove[currentOpponent]
+                );
+
+                numPiecesOnBoardCursor.reset({
+                  [currentTurn]: numberOfPiecesCurrent,
+                  [currentOpponent]: numberOfPiecesOpponent,
+                });
+
 
                 if (numberOfPiecesOpponent === 3) {
                   console.log('opponent should start flying');
