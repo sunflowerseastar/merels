77396b3701941344d221ef0ff4f58fb5789b0eb2
Grant Surlyn <grant@sunflowerseastar.com>
2023-08-04 21:43:24 -0400

    fixup - rename lifting -> removing

--- src/merelsMachine.ts
+++ src/merelsMachine.ts
@@ -71,13 +71,12 @@ export const merelsMachine = createMachine(
                     type: 'place',
                     params: {},
                   },
-                  target: 'Lifting',
+                  target: 'Removing',
                   reenter: false,
                 },
                 {
                   actions: [
                     { type: 'place' },
-                    { type: 'increment' },
                     { type: 'swap' },
                   ],
                   reenter: true,
@@ -85,22 +84,22 @@ export const merelsMachine = createMachine(
               ],
             },
           },
-          Lifting: {
+          Removing: {
             on: {
               'point.click': [
                 {
                   guard:
-                    'invalid lift (empty space || occupied by self || occupied by opponent locked in mill)',
+                    'invalid removal (empty space || occupied by self || occupied by opponent locked in mill)',
                   reenter: true,
                 },
                 {
                   guard: 'all pieces have not yet been placed',
-                  actions: [{ type: 'lift' }, { type: 'swap' }],
+                  actions: [{ type: 'remove' }, { type: 'swap' }],
                   target: 'Placing',
                   reenter: false,
                 },
                 {
-                  actions: [{ type: 'lift' }, { type: 'swap' }],
+                  actions: [{ type: 'remove' }, { type: 'swap' }],
                   target: '#merels 1.Moving.Selecting',
                   reenter: false,
                 },
@@ -146,7 +145,7 @@ export const merelsMachine = createMachine(
               },
               'valid place': [
                 {
-                  target: 'Lifting',
+                  target: 'Removing',
                   guard: 'mill is formed',
                   reenter: false,
                 },
@@ -157,17 +156,17 @@ export const merelsMachine = createMachine(
               ],
             },
           },
-          Lifting: {
+          Removing: {
             on: {
-              'valid lift': [
+              'valid removal': [
                 {
                   target: '[STATE] swap players',
-                  guard: 'opponent has more than 3 pieces remaining after lift',
+                  guard: 'opponent has more than 3 pieces remaining after removal',
                   reenter: false,
                 },
                 {
                   target: '[STATE] opponent is now flying',
-                  guard: 'opponent has 3 pieces remaining after lift',
+                  guard: 'opponent has 3 pieces remaining after removal',
                   reenter: false,
                 },
                 {
@@ -175,9 +174,9 @@ export const merelsMachine = createMachine(
                   reenter: false,
                 },
               ],
-              'invalid lift (empty space || occupied by self || occupied by opponent locked in mill)':
+              'invalid removal (empty space || occupied by self || occupied by opponent locked in mill)':
                 {
-                  target: 'Lifting',
+                  target: 'Removing',
                   reenter: false,
                 },
             },
@@ -200,11 +199,8 @@ export const merelsMachine = createMachine(
       test: ({ action }) => {
         // console.log(action.params);
       },
-      increment: ({ action }) => {
-        console.log('increment');
-      },
-      lift: ({ action }) => {
-        console.log('lift');
+      remove: ({ action }) => {
+        console.log('remove');
       },
       place: assign({
         boards: ({ context: { boards, turn }, event: { aplIndex } }) => ({
@@ -220,29 +216,16 @@ export const merelsMachine = createMachine(
     guards: {
       'point is occupied': ({ event: { pieceAtPoint } }) => !!pieceAtPoint,
       'form mill': ({ context: { boards, turn }, event: { aplIndex } }) => {
-        // console.log('guard: form mill', boards, turn, aplIndex);
-
-        // TODO determine if this forms a mill. If so, return true. Otherwise,
-        // return false and then let the next event else happen
-
         const previousNumberOfMills = getNumberOfMills(boards[turn]);
-        // console.log('previousNumberOfMills', previousNumberOfMills);
-
         const boardAfterPlace = aplPlacePiece(boards[turn], aplIndex);
-        // console.log('boardAfterPlace', boardAfterPlace);
-
         const newNumberOfMills = getNumberOfMills(boardAfterPlace);
-        // console.log('newNumberOfMills', newNumberOfMills);
-
         const isMillFormed = newNumberOfMills > previousNumberOfMills;
-        // console.log('isMillFormed', isMillFormed);
-
         return isMillFormed;
       },
-      'invalid lift (empty space || occupied by self || occupied by opponent locked in mill)':
+      'invalid removal (empty space || occupied by self || occupied by opponent locked in mill)':
         ({ event: { boards, pieceAtPoint, turn }, event: { aplIndex } }) => {
           console.log(
-            'invalid lift (empty space || occupied by self || occupied by opponent locked in mill)',
+            'invalid removal (empty space || occupied by self || occupied by opponent locked in mill)',
             boards,
             pieceAtPoint,
             turn,
@@ -269,8 +252,8 @@ export const merelsMachine = createMachine(
         return numPiecesPlaced < 18;
       },
       'mill is formed': createMachine({}),
-      'opponent has more than 3 pieces remaining after lift': createMachine({}),
-      'opponent has 3 pieces remaining after lift': createMachine({}),
+      'opponent has more than 3 pieces remaining after removal': createMachine({}),
+      'opponent has 3 pieces remaining after removal': createMachine({}),
     },
     delays: {},
   }
